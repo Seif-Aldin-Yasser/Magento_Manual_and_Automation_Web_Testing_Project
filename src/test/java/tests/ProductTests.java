022ab68546e1b79951fd8c5b6a10b99bcc792c77
Seif Aldin Yasser <123891774+Seif-Aldin-Yasser@users.noreply.github.com>
2025-08-04 09:57:01 +0300

    Update ProductTests.java

--- src/test/java/tests/ProductTests.java
+++ src/test/java/tests/ProductTests.java
@@ -11,14 +11,15 @@ public class ProductTests extends BaseTest {
     @Test(priority = 1, description = "Verify product search with full name and add to cart with specific options")
     public void testProductSearchWithFullNameAndAddToCart() {
         HomePage homePage = new HomePage(driver);
-        homePage.navigateToMenHoodiesAndSweatshirts(); // Navigate to the specific category
+        homePage.navigateToMenHoodiesAndSweatshirts(); 
 
-        SearchResultsPage searchResultsPage = homePage.performSearch("Mars HeatTech™ Pullover");
-        ProductPage productPage = searchResultsPage.selectFirstProduct();
+  SearchResultsPage searchResultsPage = homePage.performSearch("Mars HeatTech™ Pullover");
 
-        // Assuming Marco Lightweight Active Hoodie is the product selected
-        productPage.selectSize("M"); // Example size
-        productPage.selectColor("Black"); // Example color
+  ProductPage productPage = searchResultsPage.selectFirstProduct();
+
+        
+        productPage.selectSize("M");
+        productPage.selectColor("Black");
         productPage.clickAddToCart();
 
         Assert.assertTrue(productPage.isProductAddedToCartMessageDisplayed(), "Product added to cart message not displayed!");
@@ -31,14 +32,13 @@ public class ProductTests extends BaseTest {
         SearchResultsPage searchResultsPage = homePage.performSearch("Mar");
         ProductPage productPage = searchResultsPage.selectFirstProduct();
 
-        productPage.selectSize("M"); // Example size
-        productPage.selectColor("Black"); // Example color
+        productPage.selectSize("M");
+        productPage.selectColor("Black");
         productPage.clickAddToCart();
 
         Assert.assertTrue(productPage.isProductAddedToCartMessageDisplayed(), "Product added to cart message not displayed!");
     }
 
-    // Test Case 3: Verify invalid product search with 2 characters and "Minimum Search query length is 3" message
     @Test(priority = 3, description = "Verify invalid product search with 2 characters")
     public void testInvalidProductSearchWith2Chars() {
         HomePage homePage = new HomePage(driver);
@@ -46,7 +46,6 @@ public class ProductTests extends BaseTest {
 
         SearchResultsPage searchResultsPage = homePage.performSearch("Ma");
 
-        // This assertion will now rely on the 'searchMessageLocator' finding the correct element with 'message notice' class
         Assert.assertTrue(searchResultsPage.isMinimumQueryLengthMessageDisplayed(),
                 "Expected 'Minimum Search query length is 3' message not displayed for 2-character search!");
     }
